953f11922e1f5e2c66776765317f8c25fe48285e
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2022-08-26 23:17:27 -0400

    Add functions example

---
+++ examples/functions.rs
@@ -0,0 +1,31 @@
+
+fn main() {
+
+    basic_function();
+
+    let sum = adder(2, 3);
+    println!("The sum is {sum}");
+
+    let frac = divider(5.0, 2.0);
+    println!("The fraction is {frac}");
+}
+
+// A basic function with no inputs
+
+fn basic_function() {
+    println!("Basic function with no inputs");
+}
+
+// Use the return keyword to return a value
+
+fn adder(x: i32, y: i32) -> i32 {
+    let s = x + y;
+    return s
+}
+
+// This function uses an expression to return its value. Notice the absence of
+// a semicolon after the expression in the function.
+
+fn divider(x: f32, y: f32) -> f32 {
+    x / y
+}
