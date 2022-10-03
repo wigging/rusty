bc1e2cb0003c255669323347b62c9884b36c924a
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2022-10-02 21:48:25 -0400

    Add more examples of structs

--- examples/structs.rs
+++ examples/structs.rs
@@ -1,12 +1,35 @@
+// A basic struct
+
+struct Point {
+    x: i32,
+    y: i32
+}
+
+// A tuple struct
+
+struct PointTuple(i32, i32);
+
+// A struct with methods (functions)
+
+struct Shape {
+    width: u32,
+    height: u32
+}
+
+impl Shape {
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn volume(&self, v: u32) -> u32 {
+        self.width * self.height * v
+    }
+}
 
 fn main() {
 
-    // Regular struct
-
-    struct Point {
-        x: i32,
-        y: i32
-    }
+    // Using a basic struct
 
     let p = Point {
         x: 13,
@@ -15,11 +38,18 @@ fn main() {
 
     println!("x is {}, y is {}", p.x, p.y);
 
-    // Tuple struct
-
-    struct PointTuple(i32, i32);
+    // Using a tuple struct
 
     let pt = PointTuple(32, 16);
 
     println!("x is {}, y is {}", pt.0, pt.1);
+
+    // Using a struct with methods
+
+    let shape = Shape { width: 2, height: 3 };
+    let area = shape.area();
+    let volume = shape.volume(2);
+
+    println!("Area is: {area}");
+    println!("Volume is: {volume}");
 }
